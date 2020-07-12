81db351456ceb9d24038e719f36b8b56af4c9819
mateuszkojro <mateuszkojro@outlook.com>
2020-07-12 19:22:38 +0200

    full oauth2 i start redisig

--- oauth.js
+++ oauth.js
@@ -1,14 +1,86 @@
 //
 // przeprowadzenie oauth2
 //
-function request_auth() {
-  let callback_uri = "http://localhost:5500/callback.html";
-  let client_id = "280bd01d22924fb594c56c01bd47b077";
-  let url =
-    "https://accounts.spotify.com/authorize?client_id=" +
-    client_id +
-    "&response_type=code&redirect_uri=" +
-    callback_uri +
-    "&scope=user-read-private%20user-read-email&state=34fFs29kd09";
-  window.open(encodeURI(url));
+class oauth2 {
+  callback_uri = "http://localhost:5500";
+  client_id = "280bd01d22924fb594c56c01bd47b077";
+  client_secret = "3b365fc6cb2b4c14b64036a1bfc9b8a4";
+  code = null;
+  state = null;
+  access_token = null;
+  refresh_token = null;
+  json = null;
+
+  constructor() {}
+  has_token() {
+    if (this.access_token != null) {
+      return true;
+    }
+    return false;
+  }
+  request_auth() {
+    /*
+    let url =
+      "https://accounts.spotify.com/authorize?client_id=" +
+      this.client_id +
+      "&response_type=code&redirect_uri=" +
+      this.callback_uri +
+      "&scope=user-read-private%20user-read-email&state=34fFs29kd09";
+    */
+    let url =
+      "https://accounts.spotify.com/authorize?" +
+      new URLSearchParams({
+        client_id: this.client_id,
+        response_type: "code",
+        redirect_uri: this.callback_uri,
+        scope: "streaming user-read-email user-read-private",
+      });
+    async function close() {
+      window.close();
+    }
+    //close();
+    window.open(url);
+    //fetch(url);
+  }
+  async request_tokens() {
+    let url = "https://accounts.spotify.com/api/token";
+
+    let text_data =
+      "grant_type=authorization_code" +
+      "&code=" +
+      this.code +
+      "&redirect_uri=" +
+      this.callback_uri;
+    text_data = encodeURI(text_data);
+    let data = new URLSearchParams({
+      code: this.code,
+      grant_type: "authorization_code",
+      redirect_uri: this.callback_uri,
+      client_id: this.client_id,
+      client_secret: this.client_secret,
+    });
+    let other_params = {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: data,
+      method: "POST",
+    };
+
+    await fetch(url, other_params)
+      .then((data) => {
+        return data.json();
+      })
+      .then((res) => {
+        this.json = res;
+        console.log("this.json: " + this.json);
+        console.log("res: " + res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    this.access_token = this.json.access_token;
+    this.refresh_token = this.json.refresh_token;
+  }
 }
--- spotify.js
+++ spotify.js
@@ -1,71 +1,99 @@
 let x = null;
-window.onSpotifyWebPlaybackSDKReady = () => {
-  const token = 'BQDr7VtcFkd-FQZn96uUjRf-zZGgFPizBX4iKcV542l2FgozaTi9OBWM5DrSq-l0aVI2uFhvbT299XBpcfmKVI1dAn8ZZ1RjyWq6FbhrQmHS5NVutfYPkVUHZ_L1pMPwlPRfeIYDmobc6blU1N_vyY9RJru_47tYREHz';
+async function get_auth() {
+  let searchParams = new URLSearchParams(window.location.search);
+  let auth = new oauth2();
+  if (!searchParams.has("code")) {
+    auth.request_auth();
+  }
+  if (searchParams.has("code")) {
+    auth.code = searchParams.get("code");
+    auth.state = searchParams.get("state");
+    await auth.request_tokens();
+  }
+  return auth;
+}
+window.onSpotifyWebPlaybackSDKReady = async () => {
+  //console.log("zaczekalem na token: " + (await get_auth()).access_token);
+  const token = (await get_auth()).access_token;
+  console.log("this is your token: " + token);
   var player = new Spotify.Player({
-    name: 'Spotify server player',
-    getOAuthToken: cb => { cb(token); }
+    name: "Spotify server player",
+    getOAuthToken: (cb) => {
+      cb(token);
+    },
   });
   x = player;
   // Error handling
-  player.addListener('initialization_error', ({ message }) => { console.error(message); });
-  player.addListener('authentication_error', ({ message }) => { console.error(message); });
-  player.addListener('account_error', ({ message }) => { console.error(message); });
-  player.addListener('playback_error', ({ message }) => { console.error(message); });
+  player.addListener("initialization_error", ({ message }) => {
+    console.error(message);
+  });
+  player.addListener("authentication_error", ({ message }) => {
+    console.error(message);
+  });
+  player.addListener("account_error", ({ message }) => {
+    console.error(message);
+  });
+  player.addListener("playback_error", ({ message }) => {
+    console.error(message);
+  });
 
   // Playback status updates
-  player.addListener('player_state_changed', state => { 
-    if(!state){
-      document.getElementById("inside_spotify").innerHTML = "Nothing is curently playing";
+  player.addListener("player_state_changed", (state) => {
+    if (!state) {
+      document.getElementById("inside_spotify").innerHTML =
+        "Nothing is curently playing";
       return;
     }
 
     let {
       curent_song,
-      next_tracks: [next_track]
+      next_tracks: [next_track],
     } = state.track_window;
 
-    document.getElementById("inside_spotify").innerHTML = "curently playing: " + curent_song ;
+    document.getElementById("inside_spotify").innerHTML =
+      "curently playing: " + curent_song;
 
     console.log(state);
-     });
+  });
 
   // Ready
-  player.addListener('ready', ({ device_id }) => {
-    console.log('Ready with Device ID', device_id);
+  player.addListener("ready", ({ device_id }) => {
+    console.log("Ready with Device ID", device_id);
     //document.getElementById("inside_spotify").innerHTML = "Your device id: " + device_id;
-
   });
 
   // Not Ready
-  player.addListener('not_ready', ({ device_id }) => {
-    console.log('Device ID has gone offline', device_id);
-    document.getElementById("inside_spotify").innerHTML = "Device offline: " + device_id;
+  player.addListener("not_ready", ({ device_id }) => {
+    console.log("Device ID has gone offline", device_id);
+    document.getElementById("inside_spotify").innerHTML =
+      "Device offline: " + device_id;
   });
 
-
-  player.getCurrentState().then(state => { console.log("dziala") })
+  player.getCurrentState().then((state) => {
+    console.log("dziala");
+  });
   // Connect to the player!
   player.connect();
 };
 
-function prev(){
-  if (x != null) { 
+function prev() {
+  if (x != null) {
     x.previousTrack().then(() => {
-    console.log('Set to previous track!');
-  });
+      console.log("Set to previous track!");
+    });
   }
 }
-function stop(){ 
-  if (x != null) { 
+function stop() {
+  if (x != null) {
     x.togglePlay().then(() => {
-    console.log('Toggled playback!');
-  });
-  } 
+      console.log("Toggled playback!");
+    });
+  }
 }
-function next(){
-  if (x != null) { 
+function next() {
+  if (x != null) {
     x.nextTrack().then(() => {
-    console.log('Skipped to next track!');
-  });
+      console.log("Skipped to next track!");
+    });
   }
 }
