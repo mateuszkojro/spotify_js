4a805385eed1e5e16fe406bc90dd59a4e5c2364d
mateuszkojro <mateuszkojro@outlook.com>
2020-07-14 00:30:31 +0200

    some comments and simple refactoring

--- oauth.js
+++ oauth.js
@@ -2,7 +2,7 @@
 // przeprowadzenie oauth2
 //
 class oauth2 {
-  callback_uri = "https://mateuszkojro.github.io/spotify_js/play.html";
+  callback_uri = "http://localhost:5500/play.html";
   client_id = "280bd01d22924fb594c56c01bd47b077";
   client_secret = "3b365fc6cb2b4c14b64036a1bfc9b8a4";
   code = null;
@@ -12,21 +12,8 @@ class oauth2 {
   json = null;
 
   constructor() {}
-  has_token() {
-    if (this.access_token != null) {
-      return true;
-    }
-    return false;
-  }
+
   request_auth() {
-    /*
-    let url =
-      "https://accounts.spotify.com/authorize?client_id=" +
-      this.client_id +
-      "&response_type=code&redirect_uri=" +
-      this.callback_uri +
-      "&scope=user-read-private%20user-read-email&state=34fFs29kd09";
-    */
     let url =
       "https://accounts.spotify.com/authorize?" +
       new URLSearchParams({
@@ -36,21 +23,15 @@ class oauth2 {
         scope:
           "streaming user-read-email user-read-private user-modify-playback-state user-read-playback-state ",
       });
-    async function close() {
-      window.close();
-    }
+
     window.location.assign(url);
   }
+
   async request_tokens() {
+    //url to make request to
     let url = "https://accounts.spotify.com/api/token";
 
-    let text_data =
-      "grant_type=authorization_code" +
-      "&code=" +
-      this.code +
-      "&redirect_uri=" +
-      this.callback_uri;
-    text_data = encodeURI(text_data);
+    //setup POST request data and encode it as x-www-form-urlencoded
     let data = new URLSearchParams({
       code: this.code,
       grant_type: "authorization_code",
@@ -58,6 +39,7 @@ class oauth2 {
       client_id: this.client_id,
       client_secret: this.client_secret,
     });
+    //prepare headers for the request
     let other_params = {
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
@@ -66,14 +48,15 @@ class oauth2 {
       method: "POST",
     };
 
+    // send a POST request -> in the response you get a token and refresh token
     await fetch(url, other_params)
       .then((data) => {
         return data.json();
       })
       .then((res) => {
+        //HACK: da sie lepiej niz tak zrobic pozniej
+        //save response JSON
         this.json = res;
-        console.log("this.json: " + this.json);
-        console.log("res: " + res);
       })
       .catch((error) => {
         console.log(error);
@@ -82,8 +65,19 @@ class oauth2 {
     this.access_token = this.json.access_token;
     this.refresh_token = this.json.refresh_token;
   }
-}
 
+  has_token() {
+    if (this.access_token != null) {
+      return true;
+    }
+    return false;
+  }
+}
+//
+// Using custom oauth class to get a token
+//
+// TODO: should implement token refresh
+//
 async function get_auth() {
   let searchParams = new URLSearchParams(window.location.search);
   let auth = new oauth2();
