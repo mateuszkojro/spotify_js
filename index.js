32fd14fa3cc5c3aa367f1a302d1b3cebc1d2095e
mateuszkojro <mateuszkojro@outlook.com>
2020-07-15 19:26:52 +0200

    node js cd.

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require("express");
 const port = 3000;
 
 let oauth = require("oauth.js");
+const auth = oauth.oauth();
 
-//app.use('/', express.static('main'));
-app.use("/", (req, res) => {});
+app.use("/", (req, res) => {
+  app.render("index.html", (err) => {
+    res.send(`Server error: ${err}`);
+  });
+});
+app.use("/callback", (req, res) => {
+  auth.request_token();
+});
 app.post("/", (req, res) => {
   res.send(`Got a POST method ${req.text}`);
   console.log(req);
