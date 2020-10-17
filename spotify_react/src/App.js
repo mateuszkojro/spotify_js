e7408b55a009d7ad5612317ed16dcb5c7ccfc17e
mateuszkojro <mateuszkojro@outlook.com>
2020-10-17 21:57:29 +0200

    automated add

---
+++ spotify_react/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+let oauth = require("./oauth.js");
+
+const auth = new oauth.oauth2();
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
